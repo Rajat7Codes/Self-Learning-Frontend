f167f1a9b2b2d4be9e2ae65d8422dadebf5fb4cf
Rajat Patil <patilrajat805@gmail.com>
2025-08-02 23:48:29 +0530

    removed useless

--- src/components/TopicItemCard.tsx
+++ src/components/TopicItemCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import type { TaskItem } from '../pages/TasksPage';
 import { Difficulty } from '../util/Difficulty';
 import { ProgressStatus } from '../util/ProgressStatus';
--- src/pages/ProgressPage.tsx
+++ src/pages/ProgressPage.tsx
@@ -1,52 +1,5 @@
 import React from 'react';
 
-interface TopicItem {
-  title: string;
-  done: boolean;
-}
-
-interface Topic {
-  title: string;
-  completed: number;
-  total: number;
-  items: TopicItem[];
-}
-
-interface Division {
-  title: string;
-  topics: Topic[];
-}
-
-interface Section {
-  section: string;
-  percentage: number;
-  divisions: Division[];
-}
-
-const progressData: Section[] = [
-  {
-    section: 'Java',
-    percentage: 70,
-    divisions: [
-      {
-        title: 'OOP',
-        topics: [
-          {
-            title: 'Abstraction',
-            completed: 2,
-            total: 3,
-            items: [
-              { title: 'abstract keyword', done: true },
-              { title: 'real-world example', done: false },
-              { title: 'interface use-case', done: true },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
-
 const ProgressPage: React.FC = () => {
   return (
     <div></div>
